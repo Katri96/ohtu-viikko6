f90a29b57595669c794fe23e7f55039597457821
Katri <katsu4@hotmail.com>
2017-12-10 22:41:39 +0200

    tehtävät 3 ja 4

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -4,15 +4,16 @@ import statistics.matcher.*;
 
 public class Main {
     public static void main(String[] args) {
-        Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-          
-        Matcher m = new And( new HasAtLeast(10, "goals"),
-                             new HasAtLeast(10, "assists"),
-                             new PlaysIn("PHI")
-        );
-        
-        for (Player player : stats.matches(m)) {
-            System.out.println( player );
-        }
+    Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
+ 
+    QueryBuilder query = new QueryBuilder();
+ 
+    Matcher m = query.playsIn("NYR")
+                     .hasAtLeast(10, "goals")
+                     .hasFewerThan(25, "goals").build();
+ 
+    for (Player player : stats.matches(m)) {
+        System.out.println( player );
+    }
     }
 }
